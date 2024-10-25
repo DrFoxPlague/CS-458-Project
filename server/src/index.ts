0ea6c13834e6d6242e481bcc55547516b82d3a37
DrFoxPlague <dhdonner66@gmail.com>
2024-10-25 16:10:30 -0700

    Starting to set up the server

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,3 +4,6 @@ import gql from "graphql-tag";
 import { ApolloServer } from "@apollo/server";
 import { expressMiddleware } from "@apollo/server/express4";
 import mongoose from "mongoose";
+
+const port = process.env.PORT || 3000;  // assign a random port if 3000 is not available
+const app = express();
