0f7c48e02b8e7b001638c4c5946674b062b72d6c
DrFoxPlague <dhdonner66@gmail.com>
2024-10-27 03:28:47 -0700

    created resolvers folder + /resolvers/main.ts

---
+++ server/src/resolvers/main.ts
@@ -0,0 +1,7 @@
+// MAIN FILE FOR ALL RESOLVERS(tells what the schemas should do)
+
+// To-Do:
+/* 
+    - Create mongoose schemas under /server/src/schemas     **I HAVEN'T MADE THE FOLDER YET, LOL!!!**
+    - Actually create the resolvers... :P
+*/
