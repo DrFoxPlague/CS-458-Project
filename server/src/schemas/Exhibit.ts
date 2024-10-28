79e2302e9e88da81890fba93491265f25bc4a5f7
DrFoxPlague <dhdonner66@gmail.com>
2024-10-28 13:35:50 -0700

    Created exhibit mongoose schema

--- server/src/schemas/Exhibit.ts
+++ server/src/schemas/Exhibit.ts
@@ -1,3 +1,43 @@
 import { model, Schema, type InferSchemaType } from "mongoose";
 
-// unfinished...obviously lol
+const ExhibitSchema = new Schema({
+    id: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    ex_name: {
+        type: String,
+        required: true
+    },
+    content: {
+        title: {
+            type: String,
+            default: null
+        },
+        body: {
+            type: String,
+            default: null
+        },
+        image: {
+            type: String,
+            default: null
+        },
+        audio: {
+            type: String,
+            default: null
+        },
+        video: {
+            type: String,
+            default: null
+        }
+    }
+})
+
+export type IExhibit = InferSchemaType<typeof ExhibitSchema>;
+
+const ExhibitModel = model("exhibits", ExhibitSchema);
+
+export type ExhibitDocument = ReturnType<(typeof ExhibitModel)["hydrate"]>;
+
+export default ExhibitModel;
