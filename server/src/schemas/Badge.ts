e723e7a2f77afb4a4d48676a3d40f2d43a07242d
DrFoxPlague <dhdonner66@gmail.com>
2024-10-28 13:44:16 -0700

    Created badge mongoose schema

--- server/src/schemas/Badge.ts
+++ server/src/schemas/Badge.ts
@@ -1,3 +1,25 @@
 import { model, Schema, type InferSchemaType } from "mongoose";
 
-// unfinished...obviously lol
+const BadgeSchema = new Schema({
+    id: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    bdg_name: {
+        type: String,
+        required: true
+    },
+    bdg_type: {
+        type: String,
+        required: true
+    },
+})
+
+export type IBadge = InferSchemaType<typeof BadgeSchema>;
+
+const BadgeModel = model("badges", BadgeSchema);
+
+export type BadgeDocument = ReturnType<(typeof BadgeModel)["hydrate"]>;
+
+export default BadgeModel;
