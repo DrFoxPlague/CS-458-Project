496311eaf0f4b718133fb315b0ce6407137e630b
DrFoxPlague <dhdonner66@gmail.com>
2024-10-28 15:00:41 -0700

    created game mongoose schema

--- server/src/schemas/Game.ts
+++ server/src/schemas/Game.ts
@@ -1,3 +1,29 @@
 import { model, Schema, type InferSchemaType } from "mongoose";
 
-// unfinished...obviously lol
+const GameSchema = new Schema({
+    id: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    game_name: {
+        type: String,
+        required: true
+    },
+    game_type: {
+        type: String,
+        required: true
+    },
+    questions: {
+        type: Array,
+        default: null
+    }
+})
+
+export type IGame = InferSchemaType<typeof GameSchema>;
+
+const GameModel = model("games", GameSchema);
+
+export type GameDocument = ReturnType<(typeof GameModel)["hydrate"]>;
+
+export default GameModel;
