b00e921c24436fca4eca61e25403a706e612d7aa
DrFoxPlague <dhdonner66@gmail.com>
2024-10-28 15:54:12 -0700

    Created users mongoose schema

--- server/src/schemas/User.ts
+++ server/src/schemas/User.ts
@@ -1,3 +1,41 @@
 import { model, Schema, type InferSchemaType } from "mongoose";
 
-// unfinished...obviously lol
+const UserSchema = new Schema({
+    id: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    address: {
+        type: String,
+        required: true,
+    },
+    grade: {
+        type: String,
+        required: true,
+    },
+    dob: {
+        type: Date,
+        required: true,
+    },
+    bdg_coll: {
+        type: Array,
+        required: true,
+    }
+})
+
+export type IUser = InferSchemaType<typeof UserSchema>;
+
+const UserModel = model("users", UserSchema);
+
+export type UserDocument = ReturnType<(typeof UserModel)["hydrate"]>;
+
+export default UserModel;
