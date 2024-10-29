8c47c1a856dc0330f96b30f08f7a06bfc1c35ef6
DrFoxPlague <dhdonner66@gmail.com>
2024-10-29 09:09:07 -0700

    Created staff mongoose schema

--- server/src/schemas/Staff.ts
+++ server/src/schemas/Staff.ts
@@ -1,3 +1,29 @@
 import { model, Schema, type InferSchemaType } from "mongoose";
 
-// unfinished...obviously lol
+const StaffSchema = new Schema({
+    id: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    phone_num: {
+        type: String,
+        required: true,
+    }
+})
+
+export type IStaff = InferSchemaType<typeof StaffSchema>;
+
+const StaffModel = model("staff", StaffSchema);
+
+export type StaffDocument = ReturnType<(typeof StaffModel)["hydrate"]>;
+
+export default StaffModel;
