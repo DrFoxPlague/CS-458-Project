b3e1e01dced6f30812b29994f300e0b17aa2485c
DrFoxPlague <dhdonner66@gmail.com>
2024-11-01 16:39:50 -0700

    Updating exhibits resolver formatting to match the rest

--- server/src/resolvers/exhibits.ts
+++ server/src/resolvers/exhibits.ts
@@ -29,162 +29,156 @@ export default {
         createExhibit: async (
             _: any, 
             {ex_name, content}: {ex_name: string, content: 
-                {title?: string, body?: string, media?: any}}
+                {title?: string, body?: string, media?: any}},
+            context: { staff: { id: string } }
         ) => {
-
-            if (!content.title && !content.body) {
-                throw new GraphQLError("Exhibit cannot be empty!", {
-                    extensions: {
-                        errors: [
-                            {
-                                type: "content",
-                                message: "Exhibit cannot be empty!"
-                            }
-                        ]
-                    }
-                });
-            }
-
-            const { title, body, media } = content;
-            let mediaFile = null;
-
             try {
+                if (!context.staff) {
+                    throw new GraphQLError("Not authenticated!", {
+                        extensions: {
+                            errors: [
+                                {
+                                    type: "authentication",
+                                    message: "Staff member not authenticated!"
+                                }
+                            ]
+                        }
+                    });
+                }
+
+                if (!content.title && !content.body) {
+                    throw new GraphQLError("Exhibit cannot be empty!", {
+                        extensions: {
+                            errors: [
+                                {
+                                    type: "content",
+                                    message: "Exhibit cannot be empty!"
+                                }
+                            ]
+                        }
+                    });
+                }
+
+                const { title, body, media } = content;
+                let mediaFile = null;
+
                 if (media) {
                     mediaFile = await media;
                 }
-            }
-            catch(_) {
-                throw new GraphQLError("Media load error!", {
-                    extensions: {
-                        errors: [
-                            {
-                                type: "media",
-                                message: "Media load error!"
-                            }
-                        ]
+
+                const exhibit = new ExhibitModel({
+                    id: GenID.exhibit(),
+                    ex_name,
+                    content: {
+                        title,
+                        body,
+                        media: mediaFile
                     }
                 });
+
+                await exhibit.save();
+
+                return exhibit;
+
+            } catch (err) {
+                throw err;
             }
-
-            const exhibit = new ExhibitModel({
-                id: GenID.exhibit(),
-                ex_name,
-                content: {
-                    title,
-                    body,
-                    media: mediaFile
-                }
-            });
-
-            await exhibit.save();
-
-            return exhibit;
         },
         updateExhibit: async (
             _: any,
             { id, ex_name, content }:
             { id: string, ex_name?: string, content?: 
-                {title?: string, body?: string, media?: any}}
+                {title?: string, body?: string, media?: any}},
+            context: { staff: { id: string } }    
         ) => {
-            const exhibit = await ExhibitModel.findById(id);
-
-            if (!exhibit) {
-                throw new GraphQLError("Exhibit not found!", {
-                    extensions: {
-                        errors: [
-                            {
-                                type: "not_found",
-                                message: "Exhibit not found! Please check the ID."
-                            }
-                        ]
-                    }
-                });
-            }
-
-            if (ex_name) {
-                exhibit.ex_name = ex_name;
-            }
-
-            if (content) {
-                const { title, body, media } = content;
-
-                if (title) {
-                    content.title = title;
-                }
-                if (body) {
-                    content.body = body;
-                }
-                if (media) {
-                    try {
-                        content.media = await media;
-                    } catch (error) {
-                        throw new GraphQLError("Media load error!", {
-                            extensions: {
-                                errors: [
-                                    {
-                                        type: "media",
-                                        message: "Media load error!"
-                                    }
-                                ]
-                            }
-                        });
-                    }
-                }
-            }
-
             try {
+                if (!context.staff) {
+                    throw new GraphQLError("Not authenticated!", {
+                        extensions: {
+                            errors: [
+                                {
+                                    type: "authentication",
+                                    message: "Staff member not authenticated!"
+                                }
+                            ]
+                        }
+                    });
+                }
+
+                const exhibit = await ExhibitModel.findById(id);
+
+                if (!exhibit) {
+                    throw new GraphQLError("Exhibit not found!", {
+                        extensions: {
+                            errors: [
+                                {
+                                    type: "not_found",
+                                    message: "Exhibit not found! Please check the ID."
+                                }
+                            ]
+                        }
+                    });
+                }
+
+                if (ex_name) exhibit.ex_name = ex_name;
+
+                if (content) {
+                    const { title, body, media } = content;
+
+                    if (title) content.title = title;
+                    if (body) content.body = body;
+                    if (media) content.media = await media;
+
+                }
+
                 await exhibit.save();
+                
             } catch (err) {
-                throw new GraphQLError("Error saving exhibit! Please try again.", {
-                    extensions: {
-                        errors: [
-                            {
-                                type: "database",
-                                message: "Error saving exhibit! Please try again."
-                            }
-                        ]
-                    }
-                });
-        }
+                throw err;
+            }
         },
+
         deleteExhibit: async (
             _: any,
-            { id }: { id: string }
+            { id }: { id: string },
+            context: { staff: { id: string } }
         ) => {
-
-            const exhibit = await ExhibitModel.findById(id);
-
-            if (!exhibit) {
-                throw new GraphQLError("Exhibit not found!", {
-                    extensions: {
-                        errors: [
-                            {
-                                type: "not_found",
-                                message: "Exhibit not found! Please check the ID."
-                            }
-                        ]
-                    }
-                });
-            }
-
             try {
-                await ExhibitModel.findByIdAndDelete({ id });
-            } 
-            catch (err) {
-                throw new GraphQLError("Error deleting exhibit!", {
-                    extensions: {
-                        errors: [
-                            {
-                                type: "database",
-                                message: "Error deleting exhibit!"
-                            }
-                        ]
-                    }
-                });
-        } 
-        return exhibit;
-    }
+                if (!context.staff) {
+                    throw new GraphQLError("Not authenticated!", {
+                        extensions: {
+                            errors: [
+                                {
+                                    type: "authentication",
+                                    message: "Staff member not authenticated!"
+                                }
+                            ]
+                        }
+                    });
+                }
 
-       
+                const exhibit = await ExhibitModel.findById(id);
+
+                if (!exhibit) {
+                    throw new GraphQLError("Exhibit not found!", {
+                        extensions: {
+                            errors: [
+                                {
+                                    type: "not_found",
+                                    message: "Exhibit not found! Please check the ID."
+                                }
+                            ]
+                        }
+                    });
+                }
+
+                await ExhibitModel.findByIdAndDelete({ id });
+
+                return exhibit;
+
+            } catch (err) {
+                throw err;
+            }
+        }
     }
 }
