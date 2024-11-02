61b6e818908ae16b8d404e6df8051241797ba78d
DrFoxPlague <dhdonner66@gmail.com>
2024-11-02 16:43:29 -0700

    Updating GQL schemas + base games resolvers

--- server/src/resolvers/games/games.ts
+++ server/src/resolvers/games/games.ts
@@ -3,7 +3,8 @@ import { GraphQLError } from "graphql";
 
 export default {
     Query: {
-
+        getGame: async () => {},
+        getGames: async () => {}
     },
     Mutation: {
         createGame: async () => {},
