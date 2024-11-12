f507a61bdbdd66ece621d0080e980b2a20bf535b
DrFoxPlague <dhdonner66@gmail.com>
2024-11-12 15:41:23 -0800

    Finishing exhibits frontend GQL structure

--- client/src/gql/exhibits.tsx
+++ client/src/gql/exhibits.tsx
@@ -1,1 +1,81 @@
 import { gql } from "@apollo/client";
+
+export const CreateExhibit = gql`
+    mutation CreateExhibit($ex_name: String!, $content: ExhibitContentInput!) {
+        createExhibit(ex_name: $ex_name, content: $content) {
+            id
+            ex_name
+            content {
+                title
+                body
+                image
+                audio
+                video
+            }
+        }
+    }
+`;
+
+export const UpdateExhibit = gql`
+    mutation UpdateExhibit($id: ID!, $ex_name: String, $content: ExhibitContentInput) {
+        updateExhibit(id: $id, ex_name: $ex_name, content: $content) {
+            id
+            ex_name
+            content {
+                title
+                body
+                image
+                audio
+                video
+            }
+        }
+    }
+`;
+
+export const DeleteExhibit = gql`
+    mutation DeleteExhibit($id: ID!) {
+        deleteExhibit(id: $id) {
+            id
+            ex_name
+            content {
+                title
+                body
+                image
+                audio
+                video
+            }
+        }
+    }
+`;
+
+export const GetExhibit = gql`
+    query GetExhibit($id: ID!) {
+        getExhibit(id: $id) {
+            id
+            ex_name
+            content {
+                title
+                body
+                image
+                audio
+                video
+            }
+        }
+    }
+`;
+
+export const GetExhibits = gql`
+    query GetExhibits {
+        getExhibits {
+            id
+            ex_name
+            content {
+                title
+                body
+                image
+                audio
+                video
+            }
+        }
+    }
+`;
