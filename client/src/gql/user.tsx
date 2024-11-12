b0fcbf8ab7473a60c140ce06e7c193649cfcedee
DrFoxPlague <dhdonner66@gmail.com>
2024-11-12 14:52:30 -0800

    Progress on user GQL frontend stuff

--- client/src/gql/user.tsx
+++ client/src/gql/user.tsx
@@ -1,1 +1,19 @@
 import { gql } from "@apollo/client";
+
+export const CreateOrUpdateUser = gql`
+    mutation CreateUser($id: ID!, $input: UserInput!) {
+        createUser(id: $id, input: $input) {
+            id
+            name
+            email
+            grade
+            dob
+            bdg_coll {
+                id
+                bdg_name
+                bdg_type
+            }
+        }
+    }
+`;
+
