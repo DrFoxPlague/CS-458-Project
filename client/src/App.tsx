b511e15a0eaef6734f9e4b187c4e32bad8aa5d77
DrFoxPlague <dhdonner66@gmail.com>
2024-11-13 11:36:18 -0800

    Set up apollo client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,19 @@
 //peter
 import './App.css';
 import { TestGame } from './components/TestGame';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
+const client = new ApolloClient({
+    uri: 'http://localhost:4000/graphql',  // Your GraphQL server URL
+    cache: new InMemoryCache(),
+    credentials: 'include', // Allows cookies for auth if using sessions
+});
 
 
 const App = () => (
-    <div>
-    <TestGame />
-    </div>
+    <ApolloProvider client={client}>
+        <TestGame />
+    </ApolloProvider>
 )
 
 export default App;
