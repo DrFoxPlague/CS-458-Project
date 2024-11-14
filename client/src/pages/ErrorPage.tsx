dc726b8b79d2d40f7a315dc5a329db6e7f64dc59
DrFoxPlague <dhdonner66@gmail.com>
2024-11-14 11:11:06 -0800

    Added tailwind css functionality

--- client/src/pages/ErrorPage.tsx
+++ client/src/pages/ErrorPage.tsx
@@ -2,8 +2,8 @@ import '../css/App.css';
 
 export const ErrorPage = () => {
     return (
-        <div className="ErrorPage">
-            <h1>404</h1>
+        <div className="flex items-center justify-center h-screen">
+            <h1 className="font-bold text-balance">404</h1>
             <p>How'd you get here?</p>
         </div>
     )
