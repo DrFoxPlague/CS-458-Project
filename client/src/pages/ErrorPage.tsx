7eb1c498e30a16112308984c569d4b4a8bb27fa2
DrFoxPlague <dhdonner66@gmail.com>
2024-11-14 11:37:44 -0800

    Changed global CSS

--- client/src/pages/ErrorPage.tsx
+++ client/src/pages/ErrorPage.tsx
@@ -1,10 +1,8 @@
-import '../css/App.css';
-
 export const ErrorPage = () => {
     return (
-        <div className="flex items-center justify-center h-screen">
-            <h1 className="font-bold text-balance">404</h1>
-            <p>How'd you get here?</p>
+        <div className="flex flex-col items-center justify-center w-full h-screen text-center">
+            <h1 className="text-6xl font-bold">404</h1>
+            <p className="text-lg mt-4">How'd you get here?</p>
         </div>
     )
 }
