fe53ca503dd68c0fa9add30d5042287a76f2fe83
DrFoxPlague <dhdonner66@gmail.com>
2024-11-20 16:26:19 -0800

    Fixing tailwind + adding toast functionality + progress on login page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,7 +5,8 @@ import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import {BrowserRouter as Router, Routes, Route, Link} from "react-router-dom"
 import { ErrorPage } from './pages/ErrorPage';
 import { ExhibitLayout } from './exhibits/ExhibitLayout';
-import { LoginPage } from './pages/LoginPage';
+import { LoginPage, FollowUpPage } from './pages/LoginPage';
+import { Toaster } from 'react-hot-toast';
 
 const client = new ApolloClient({
     uri: 'http://localhost:4000/graphql',  // Your GraphQL server URL
@@ -16,12 +17,25 @@ const client = new ApolloClient({
 
 const App = () => (
     <ApolloProvider client={client}>
+        <Toaster 
+            position="bottom-center"
+            toastOptions={{
+            // Define default options
+                className: '',
+                duration: 5000,
+                style: {
+                    background: '#363636',
+                    color: '#fff',
+                }
+            }}
+        />
         <Router>
             <Routes>
                 <Route path="*" element={<ErrorPage />}/>
                 <Route path="/" element={<TestGame />}/>
                 <Route path="/exhibit" element={<ExhibitLayout />}/>
                 <Route path="/login" element={<LoginPage />}/>
+                <Route path="/signup" element={<FollowUpPage />}/>
             </Routes>
         </Router>
     </ApolloProvider>
--- client/src/pages/LoginPage.tsx
+++ client/src/pages/LoginPage.tsx
@@ -1,5 +1,9 @@
 import image from "../assets/cits-logo.png"
-import { Card, Stack, Button } from "@mui/material"
+import { Card, Stack, Button, InputLabel, Select, FormControl, MenuItem } from "@mui/material"
+import { useMutation } from "@apollo/client";
+import { CreateOrUpdateUser } from "../gql/user";
+import { useState } from "react";
+import toast from "react-hot-toast";
 
 export const LoginPage = () => {
 
@@ -40,6 +44,106 @@ export const LoginPage = () => {
                 </div>
             </div>
         </Card>
+      </Stack>
+    )
+}
+
+export const FollowUpPage = () => {
+    const [signUpData, setSignUpData] = useState({
+        dob: "",
+        grade: "",
+    });
+
+    const [createUser, { loading }] = useMutation(CreateOrUpdateUser);
+
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault();
+
+        if (!signUpData.dob || !signUpData.grade) return toast.error("Please fill out ALL fields!");
+
+        try {
+            await createUser({
+                variables: {
+                    input: signUpData
+                }
+            });
+            
+        } catch (err) {
+            console.error("Error logging in:", err);
+        }
+    };
+
+    return (
+        <Stack className="w-full max-w-xl mx-auto p-4 space-y-4">
+            <Card className="rounded-lg shadow-md overflow-hidden">
+                <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+                    <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+                        <img
+                            alt="CPH Logo"
+                            src={image}
+                            className="mx-auto h-15 w-15"
+                        />
+                        <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900">
+                            Finish Signing Up
+                        </h2>
+
+                        <form onSubmit={handleSubmit} className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm space-y-6">
+                            <div>
+                                <InputLabel id="grade-label" className="block text-sm font-bold text-gray-900">Grade</InputLabel>
+                                <FormControl fullWidth>
+                                    <Select
+                                        labelId="grade-label"
+                                        name="grade"
+                                        value={signUpData.grade}
+                                        onChange={(e) =>
+                                            setSignUpData({
+                                                ...signUpData,
+                                                grade: e.target.value,
+                                            })
+                                        }
+                                        label="Grade"
+                                        className="mt-2 w-full py-1.5 rounded-md border-0 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm"
+                                    >
+                                        <MenuItem value="Elementary">Elementary</MenuItem>
+                                        <MenuItem value="HighSchool">High School</MenuItem>
+                                        <MenuItem value="College">College</MenuItem>
+                                    </Select>
+                                </FormControl>
+                            </div>
+
+                            <div>
+                                <InputLabel id="dob-label" className="block text-sm font-bold text-gray-900">Date of Birth</InputLabel>
+                                <input
+                                    type="date"
+                                    id="dob"
+                                    name="dob"
+                                    value={signUpData.dob}
+                                    onChange={(e) =>
+                                        setSignUpData({
+                                            ...signUpData,
+                                            dob: e.target.value,
+                                        })
+                                    }
+                                    className="bg-white mt-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm"
+                                />
+                            </div>
+
+                            {/* Submit Button */}
+                            <div>
+                                <Button
+                                    type="submit"
+                                    variant="contained"
+                                    color="primary"
+                                    className="w-full !bg-green-800 hover:bg-indigo-500 text-white"
+                                    disabled={loading}
+                                >
+                                    {loading ? "Submitting..." : "Finish Signup"}
+                                </Button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </Card>
       </Stack>
     )
 }
