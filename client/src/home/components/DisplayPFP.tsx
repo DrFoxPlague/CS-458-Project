e50c930a846afc795f7db54232debc9d2e78a3e5
DrFoxPlague <dhdonner66@gmail.com>
2024-12-02 15:33:40 -0800

    New package + scrapping old code

--- client/src/home/components/DisplayPFP.tsx
+++ client/src/home/components/DisplayPFP.tsx
@@ -7,42 +7,11 @@ import toast from "react-hot-toast";
 export const DisplayPFP = () => {
     const [userId, setUserId] = useState(null);
     
-    useEffect(() => {
-        const fetchUserId = async () => {
-            try {
-                const response = await fetch("http://localhost:4000/auth/me", {
-                    credentials: "include", // Ensure cookies are sent
-                });
-
-                if (response.ok) {
-                    //const data = await response.json();
-                    setUserId(data._id);
-                } else {
-                    console.error("Failed to fetch user ID. Please log in.");
-                    // Redirect to login page or handle unauthenticated state
-                }
-            } catch (err) {
-                console.error("Error fetching user ID:", err);
-            }
-        };
-
-        fetchUserId();
-    }, []);
-
-    const { data, loading, error } = useQuery(GetUser, {
-        variables: { id: userId },
-        skip: !userId, // Skip query until userId is set
-    });
-
-    if (loading) return toast.loading("Retrieving user info...");
-    if (error) {
-        console.error("Error retrieving information: ", error);
-        return toast.error("Error retrieving information!")
-    }
-
-    const user = data?.getUser;
+    // SCRAPPED CODE, REMAKING
     
     return (
-        <Avatar alt={user.name} src={user.prof_pic}/>
+        //<Avatar alt={user.name} src={user.prof_pic}/>
+
+        console.log(userId)
     )
 }
