9a3cf4ae24b076e0ec8c200371d8882178a930a1
DrFoxPlague <dhdonner66@gmail.com>
2024-12-03 09:21:32 -0800

    Changed store code

--- client/src/stores/Auth.store.tsx
+++ client/src/stores/Auth.store.tsx
@@ -1,22 +1,21 @@
 import { create } from "zustand";
-import { AuthState } from "../types/auth";
+import { User } from "../types/auth";
+import { persist } from "zustand/middleware";
 
-export const useAuthStore = create<AuthState>((set) => ({
-    user: null,
-    setUser: async () => {
-        try {
-            const response = await fetch("http://localhost:4000/auth/me", {
-                credentials: "include", // Ensure cookies/session are sent
-            });
+type State = {
+    user: User | null;
+};
 
-            if (response.ok) {
-                const user = await response.json();
-                set({ user }); // Set user in the store
-            } else {
-                console.error("Failed to fetch authenticated user");
-            }
-        } catch (err) {
-            console.error("Error fetching user:", err);
-        }
-    }
-}));
+type Action = {
+    setUser: (user: User) => void;
+};
+
+export const useAuthStore = create<State & Action>()(
+    persist(
+        (set) => ({
+            user: null,
+            setUser: (user: User) => set({ user }),
+        }),
+        { name: "auth" }
+    )
+);
